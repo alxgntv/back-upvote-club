5b518428da95d571736229d11c31d7aa9b773f59
alxgntv <support@code.market>
2026-01-22 10:14:57 +0530

    Update

--- buddyboost/settings.py
+++ buddyboost/settings.py
@@ -114,7 +114,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
         'LOCATION': 'django_cache_table',
-        'TIMEOUT': 31536000,  # 1 year (365 days × 24 hours × 3600 seconds)
+        'TIMEOUT': 31536000,
         'OPTIONS': {
             'MAX_ENTRIES': 10000
         }
